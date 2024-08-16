533f1a00f4c6cb6f85172f385f8b823ef40bf512
Rohit Yadav <rohityadav.se@gmail.com>
2024-08-16 20:53:09 +0545

    as

--- tokenGeneration.js
+++ tokenGeneration.js
@@ -14,9 +14,7 @@ const loginClient = async (clientId, appToken) => {
         const response = await axios.post(urls.login, payload);
         return response.data.clientToken;
     } catch (error) {
-        console.log(error.response);
-
-        throw new Error(error);
+        throw new Error('error generating client token');
     }
 }
 
