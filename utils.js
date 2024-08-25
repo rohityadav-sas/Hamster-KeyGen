f239fac7002890670d182129619a5e092d5fdaf2
Rohit Yadav <rohityadav.se@gmail.com>
2024-08-25 20:49:57 +0545

    completed fixing bugs

--- utils.js
+++ utils.js
@@ -3,22 +3,22 @@ const games = {
         appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
         promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
     },
-    // 'Clone': {
-    //     appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
-    //     promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767',
-    // },
-    // 'Bike': {
-    //     appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
-    //     promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
-    // },
-    // 'Train': {
-    //     appToken: '82647f43-3f87-402d-88dd-09a90025313f',
-    //     promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
-    // },
-    // 'Merge': {
-    //     appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
-    //     promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
-    // },
+    'Clone': {
+        appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
+        promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767',
+    },
+    'Bike': {
+        appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
+        promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
+    },
+    'Train': {
+        appToken: '82647f43-3f87-402d-88dd-09a90025313f',
+        promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
+    },
+    'Merge': {
+        appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
+        promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
+    },
     'Twerk': {
         appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
         promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c'
