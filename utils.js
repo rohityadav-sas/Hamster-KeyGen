d7027ef6149accc73a1590604b6b26e3f1a0591c
Rohit Yadav <rohityadav.se@gmail.com>
2024-08-28 18:24:48 +0545

    Update utils.js

--- utils.js
+++ utils.js
@@ -3,10 +3,6 @@ const games = {
         appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
         promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
     },
-    'Clone': {
-        appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
-        promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767',
-    },
     'Bike': {
         appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
         promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
