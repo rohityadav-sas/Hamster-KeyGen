04d080a099642b2694f41f7092bb80556c9e41cd
Rohit Yadav <rohityadav.se@gmail.com>
2024-08-28 21:14:52 +0545

    Added new game 'cafe'

--- utils.js
+++ utils.js
@@ -30,6 +30,10 @@ const games = {
     'Mud': {
         appToken: '8814a785-97fb-4177-9193-ca4180ff9da8',
         promoId: '8814a785-97fb-4177-9193-ca4180ff9da8'
+    },
+    'Cafe': {
+        appToken: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
+        promoId: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11'
     }
 }
 const urls = {
